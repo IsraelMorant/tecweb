e2efb29c3f700601869fc9dbdd8cda0082163a72
Israel Morante Luna <israelmorantl@gmail.com>
2025-09-30 14:22:18 -0600

    Utf intento de funcionar1

--- practicas/p08/get_producto_by_id.php
+++ practicas/p08/get_producto_by_id.php
@@ -8,18 +8,21 @@
 	if (!empty($id))
 	{
 		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
-
+		$link = new mysqli('localhost', 'root', '5024', 'marketzone');	
+		$link->set_charset('utf8');
 		/** comprobar la conexión */
 		if ($link->connect_errno) 
 		{
 			die('Falló la conexión: '.$link->connect_error.'<br/>');
+
+			//echo $connect_errno ."hdoald";
 			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 		}
 
 		/** Crear una tabla que no devuelve un conjunto de resultados */
 		if ( $result = $link->query("SELECT * FROM productos WHERE id = '{$id}'") ) 
 		{
+			//echo "devbus";
 			$row = $result->fetch_array(MYSQLI_ASSOC);
 			/** útil para liberar memoria asociada a un resultado con demasiada información */
 			$result->free();
@@ -29,7 +32,7 @@
 	}
 	?>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Producto</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
@@ -39,7 +42,8 @@
 		<br/>
 		
 		<?php if( isset($row) ) : ?>
-
+			
+				
 			<table class="table">
 				<thead class="thead-dark">
 					<tr>
