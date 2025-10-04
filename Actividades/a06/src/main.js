3210b5cf7e6ebf4ae7400c1a87878ffee35485dc
Israel Morante Luna <israelmorantl@gmail.com>
2025-10-04 01:51:03 -0600

    Funciones 1, 2 y 3 agregadas

--- Actividades/a06/src/main.js
+++ Actividades/a06/src/main.js
@@ -1,5 +1,5 @@
 
-function getDatos()
+function getDatos()//Funcion inicial
 {
     var nombre = prompt("Nombre: ", "");
 
@@ -10,4 +10,43 @@ function getDatos()
 
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+}
+
+function funcion1(){//Funcion 1
+
+
+    document.write("Hola mundo");
+}
+
+
+function funcion2(){//Funcion 2
+var nombre = 'Juan';
+var edad = 10;
+
+var altura = 1.92;
+
+var casado = false;
+
+
+document.write( nombre );
+document.write( '<br>' );
+document.write( edad );
+document.write( '<br>' );
+document.write( altura );
+document.write( '<br>' );
+document.write( casado );
+}
+
+
+
+function funcion3(){//Funcion 2
+var nombre;
+var edad;
+nombre = prompt('Ingresa tu nombre:', '');
+edad = prompt('Ingresa tu edad:', '');
+document.write('Hola ');
+document.write(nombre);
+document.write(' así que tienes ');
+document.write(edad);
+document.write(' años');
 }
